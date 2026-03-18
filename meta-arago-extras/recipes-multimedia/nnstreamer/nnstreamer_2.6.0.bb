PrathamTI/meta-arago

commit 319271fe5fc8bc3016b1eb765784076af666b61b

nnstreamer: upgrade 2.4.4 -> 2.6.0
We are migrating nnstreamer from v2.4.4 to the
stable release v2.6.0.

Key Changes:
 - Updated version from v2.4.4 to v2.6.0

Reviewed-by: Andrew Davis <afd@ti.com>
Signed-off-by: Pratham Deshmukh <p-deshmukh@ti.com>
Signed-off-by: Ryan Eatmon <reatmon@ti.com>

meta-arago-extras/recipes-multimedia/nnstreamer/nnstreamer_2.6.0.bb
@@ -4,7 +4,7 @@ LICENSE = "LGPL-2.1-only"
 LIC_FILES_CHKSUM = "file://LICENSE;md5=c25e5c1949624d71896127788f1ba590"
 
 SRC_URI = "git://github.com/nnstreamer/nnstreamer.git;branch=main;protocol=https"
-SRCREV = "3b32573bc104b4708af44837466b8b6c73f94869"
+SRCREV = "b970e9eff6bfb4e915463842422fe14bb2e53f84"
 
 # Only compatible with armv7a, armv7ve, and aarch64
 COMPATIBLE_MACHINE = "(^$)"

commit 94e573ba4a059f2c44234753363b5358b705da4b

nnstreamer: fix buildpaths QA issues
Include buildpaths compliance patch that replaces
absolute build paths(meson.build_root()) with relative
paths in generated unittest files, resolving QA errors
"contains reference to TMPDIR [buildpaths]" in unittest_tizen_custom.cc
and unittest_tizen_custom-set.cc.

Fixes: 319271fe5fc8bc3016b1eb765784076af666b61b ("nnstreamer: upgrade 2.4.4 -> 2.6.0")

Signed-off-by: Pratham Deshmukh <p-deshmukh@ti.com>
Signed-off-by: Ryan Eatmon <reatmon@ti.com>

meta-arago-extras/recipes-multimedia/nnstreamer/nnstreamer/0001-fix-Remove-absolute-build-paths-from-generated-test-.patch
@@ -1,0 +1,41 @@
+From a5f962b51431ac19c264b8b9baa78ff283a77efe Mon Sep 17 00:00:00 2001
+From: Pratham Deshmukh <p-deshmukh@ti.com>
+Date: Tue, 10 Mar 2026 15:23:05 +0530
+Subject: [PATCH] fix: Remove absolute build paths from generated test files
+
+Replace meson.build_root() with relative paths in custom_filter_path
+to prevent Yocto buildpaths QA errors. The generated unittest files
+were embedding absolute build directory paths (e.g., /tmp/work/...)
+as string literals, which Yocto's QA system correctly flags as a
+build reproducibility issue.
+
+The runtime code already handles proper path resolution using
+NNSTREAMER_SOURCE_ROOT_PATH environment variable or relative
+fallbacks, so this change maintains full functionality
+while eliminating embedded build paths.
+
+Fixes: QA Issue "contains reference to TMPDIR [buildpaths]" in
+unittest_tizen_custom.cc and unittest_tizen_custom-set.cc
+
+Upstream-Status: Inappropriate [oe-specific]
+
+Signed-off-by: Pratham Deshmukh <p-deshmukh@ti.com>
+---
+ tests/nnstreamer_filter_extensions_common/meson.build | 2 +-
+ 1 file changed, 1 insertion(+), 1 deletion(-)
+
+diff --git a/tests/nnstreamer_filter_extensions_common/meson.build b/tests/nnstreamer_filter_extensions_common/meson.build
+index ecd1878b..ba72ee20 100644
+--- a/tests/nnstreamer_filter_extensions_common/meson.build
++++ b/tests/nnstreamer_filter_extensions_common/meson.build
+@@ -9,7 +9,7 @@ tizen_apptest_deps = [
+ # Format for adding subplugin into extensions -
+ # [name, extension abbreviation, dependencies, model file name/folder path/file path, test name]
+ extensions = []
+-custom_filter_path = join_paths(meson.build_root(), 'tests', 'nnstreamer_example',
++custom_filter_path = join_paths('tests', 'nnstreamer_example',
+     'libnnstreamer_customfilter_passthrough.' + so_ext)
+ extensions += [['custom', 'custom', nnstreamer_unittest_deps, custom_filter_path, 'custom']]
+ extensions += [['custom', 'custom', nnstreamer_unittest_deps, custom_filter_path, 'custom-set']]
+--
+2.34.1

meta-arago-extras/recipes-multimedia/nnstreamer/nnstreamer_2.6.0.bb
@@ -3,7 +3,11 @@ DESCRIPTION = "NNStreamer is a set of Gstreamer plugins that allow Gstreamer dev
 LICENSE = "LGPL-2.1-only"
 LIC_FILES_CHKSUM = "file://LICENSE;md5=c25e5c1949624d71896127788f1ba590"
 
-SRC_URI = "git://github.com/nnstreamer/nnstreamer.git;branch=main;protocol=https"
+SRC_URI = " \
+	git://github.com/nnstreamer/nnstreamer.git;branch=main;protocol=https \
+	file://0001-fix-Remove-absolute-build-paths-from-generated-test-.patch \
+"
+
 SRCREV = "b970e9eff6bfb4e915463842422fe14bb2e53f84"
 
 # Only compatible with armv7a, armv7ve, and aarch64
@@ -62,7 +66,6 @@ FILES:${PN}-tests += "\
 	${libdir}/nnstreamer/customfilters/* \
 	${bindir}/unittest-nnstreamer/* \
 "
-INSANE_SKIP:${PN}-tests += "buildpaths"
 
 FILES:${PN}-dev = "\
 	${includedir}/nnstreamer/* \